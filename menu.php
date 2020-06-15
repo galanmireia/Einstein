db623332a0cb4bab7aa0b8e3ffdf1ebd14398776
galanmireia <55281394+galanmireia@users.noreply.github.com>
2020-06-15 17:37:06 +0200

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+<body>
+<frameset rows="80,*">
+<frame src="arriba.php" name="top" scrolling="no" />
+<frame src="abajo.php" name="main" />
+</ frameset>
+</HTML>
